3eff7808cdfc8e5853216736517e0f7ccc6d5c6a
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-08-02 01:49:41 +0300

    setup issue clearing

--- src/components/Navigations/Navigation.js
+++ src/components/Navigations/Navigation.js
@@ -9,21 +9,17 @@ const Navigation = () => {
         <Router>
             <nav className="bg-white-800 py-4 border border-white-100 rounded-lg shadow-sm">
                 <div className="container mx-auto px-4 sm:px-8 md:px-16 lg:px-20 xl:px-24 2xl:px-32 flex flex-col sm:flex-row justify-between items-center">
-                    <Link exact to="/" className="text-white mr-4 hover:text-gray-300">
+                    <Link exact to="/" className="text-gray-600 mr-4 hover:text-gray-300">
                         My Profile
                     </Link>
-                    <Link exact to="/rockets" className="text-white mr-4 hover:text-gray-300">
+                    <Link exact to="/rockets" className="text-gray-600 mr-4 hover:text-gray-300">
                         Rockets
                     </Link>
                 </div>
             </nav>
             <Routes>
-                <Route exact path="/" element={<RocketsList />} />
-                <Route exact path="/rockets" element={<RocketsList />} />
-            </Routes>
-            <Routes>
                 <Route exact path="/" element={<MissionsList />} />
-                <Route exact path="/missions" element={<MissionsList />} />
+                <Route exact path="/rockets" element={<RocketsList />} />
             </Routes>
         </Router>
     );
