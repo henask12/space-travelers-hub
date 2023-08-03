c1b16eb65d0e33b58d784b749e99e6d0638f0468
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-08-04 01:50:21 +0300

    test script for the navigation component

---
+++ src/__tests__/Navigation.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import Navigation from '../components/Navigations/Navigation';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+
+describe('Navigation component', () => {
+  it('should render with logo and links', () => {
+    const { getByText } = render(<Provider store={store}>
+        <Navigation />
+      </Provider>);
+    expect(getByText('Space Travelers Hub')).toBeInTheDocument();
+    expect(getByText('Missions')).toBeInTheDocument();
+    expect(getByText('Rockets')).toBeInTheDocument();
+    expect(getByText('My Profile')).toBeInTheDocument();
+  });
+
+  it('should render MissionsList component when path is "/"', () => {
+    const { getByText } = render( <Provider store={store}>
+        <Navigation />
+      </Provider>);
+    fireEvent.click(getByText('Missions'));
+    waitFor(() => {
+      expect(getByText('Mission 1')).toBeInTheDocument();
+    });
+  });
+
+  it('should render RocketsList component when path is "/rockets"', () => {
+    const { getByText } = render(<Provider store={store}>
+        <Navigation />
+      </Provider>);
+    fireEvent.click(getByText('Rockets'));
+    waitFor(() => {
+      expect(getByText('Rocket 1')).toBeInTheDocument();
+    });
+  });
+
+  it('should render MyProfilePage component when path is "/profile"', () => {
+    const { getByText } = render(<Provider store={store}>
+        <Navigation />
+      </Provider>);
+    fireEvent.click(getByText('My Profile'));
+    waitFor(() => {
+      expect(getByText('Profile Information')).toBeInTheDocument();
+    });
+  });
+});
